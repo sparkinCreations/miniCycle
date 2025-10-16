4d52599ee5211ac7c2123ff41a58533551ed3620
mjay0018 <sparkintechproductions@gmail.com>
2025-10-16 03:44:08 -0400

    feat(versioning): update app version to 1.326 and cache version to v103

--- web/service-worker.js
+++ web/service-worker.js
@@ -1,6 +1,6 @@
 // ES5-compatible (no const/let, no arrow funcs, no async/await, no optional chaining)
-var APP_VERSION = '1.325';
-var CACHE_VERSION = 'v102';
+var APP_VERSION = '1.326';
+var CACHE_VERSION = 'v103';
 var STATIC_CACHE = 'miniCycle-static-' + CACHE_VERSION;
 var DYNAMIC_CACHE = 'miniCycle-dynamic-' + CACHE_VERSION;
 
