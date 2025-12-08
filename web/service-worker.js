1122869022c53869ec3cd36d7b98d99531783946
mjay0018 <sparkintechproductions@gmail.com>
2025-12-08 14:44:30 -0500

    chore: update version to 1.437 across all relevant files and increment cache version to v249

--- web/service-worker.js
+++ web/service-worker.js
@@ -5,7 +5,7 @@
 
 importScripts('./version.js');
 var APP_VERSION = self.APP_VERSION;
-var CACHE_VERSION = 'v248';
+var CACHE_VERSION = 'v249';
 
 // On install, immediately take control
 self.addEventListener('install', function(event) {
