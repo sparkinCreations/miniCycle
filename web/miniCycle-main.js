b35f09b8fff77fc1ae29cdf6c4f130376e1d8bd7
mjay0018 <135385203+mjay0018@users.noreply.github.com>
2025-12-12 19:30:03 -0500

    Update APP_VERSION initialization
    Remove default APP_VERSION fallback.

--- web/miniCycle-main.js
+++ web/miniCycle-main.js
@@ -21,7 +21,7 @@
  * ============================================================================
  */
 
-const APP_VERSION = window.APP_VERSION || '1.480';
+const APP_VERSION = window.APP_VERSION;
 
 // Start the application
 (async () => {
