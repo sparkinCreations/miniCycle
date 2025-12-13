7c7fa48805370bd44b064aee163f37e4b9a1b227
mjay0018 <135385203+mjay0018@users.noreply.github.com>
2025-12-12 20:28:55 -0500

    Set default APP_VERSION if not defined

--- web/miniCycle-main.js
+++ web/miniCycle-main.js
@@ -21,7 +21,7 @@
  * ============================================================================
  */
 
-const APP_VERSION = window.APP_VERSION;
+const APP_VERSION = window.APP_VERSION || '1.0';
 
 // Start the application
 (async () => {
