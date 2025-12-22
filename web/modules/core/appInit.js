4919838bf1465aa1be9f1a5438d84b5e9e4acc1b
mjay0018 <sparkintechproductions@gmail.com>
2025-12-21 23:40:31 -0500

    chore: update version to 1.522 across all files and adjust service worker cache version

--- web/modules/core/appInit.js
+++ web/modules/core/appInit.js
@@ -10,7 +10,7 @@
  * Now includes initialSetup and completeInitialSetup methods (extracted
  * from main script).
  *
- * @version 1.521
+ * @version 1.522
  */
 
 import { createDIModule, optional } from './diBase.js';
