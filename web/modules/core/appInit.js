6f8092101550a29a052c278f4c02b98835d76788
mjay0018 <sparkintechproductions@gmail.com>
2025-12-24 03:13:44 -0500

    feat: update version to 1.553 across all relevant files and scripts

--- web/modules/core/appInit.js
+++ web/modules/core/appInit.js
@@ -10,7 +10,7 @@
  * Now includes initialSetup and completeInitialSetup methods (extracted
  * from main script).
  *
- * @version 1.552
+ * @version 1.553
  */
 
 import { createDIModule, optional } from './diBase.js';
