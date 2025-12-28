c799f00aacbcec7ef1ba1b172c9f306c6be388c5
mjay0018 <sparkintechproductions@gmail.com>
2025-12-28 11:17:54 -0500

    chore: Bump version to 1.592 across all relevant files

--- web/modules/core/appInit.js
+++ web/modules/core/appInit.js
@@ -10,7 +10,7 @@
  * Now includes initialSetup and completeInitialSetup methods (extracted
  * from main script).
  *
- * @version 1.591
+ * @version 1.592
  */
 
 import { createDIModule, optional } from './diBase.js';
