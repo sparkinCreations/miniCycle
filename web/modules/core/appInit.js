9979447cb5d707f49c8b05d633bb65de4905e34c
mjay0018 <sparkintechproductions@gmail.com>
2025-12-28 08:49:43 -0500

    feat(taskDOM): Add patchTask and removeTask to MODULE_MANIFESTS for enhanced task management

--- web/modules/core/appInit.js
+++ web/modules/core/appInit.js
@@ -10,7 +10,7 @@
  * Now includes initialSetup and completeInitialSetup methods (extracted
  * from main script).
  *
- * @version 1.586
+ * @version 1.587
  */
 
 import { createDIModule, optional } from './diBase.js';
